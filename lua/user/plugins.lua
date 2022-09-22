c6192de734cf99750db96962608fd1bfe5b75719
Jitendra Mishra <jitendra29mishra@gmail.com>
2022-09-22 08:48:01 +0530

    update packages

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -61,18 +61,17 @@ return packer.startup(function(use)
       end,
     }
 
-    use { "anuvyklack/windows.nvim",
-    requires = {
-      "anuvyklack/middleclass",
-      "anuvyklack/animation.nvim"
-    },
-    config = function()
-      vim.o.winwidth = 10
-      vim.o.winminwidth = 10
-      vim.o.equalalways = false
-      require('windows').setup()
-    end
-  }
+    -- use { "anuvyklack/windows.nvim",
+    -- requires = {
+    --   "anuvyklack/middleclass",
+    --   "anuvyklack/animation.nvim"
+    -- },
+    -- config = function()
+    --   vim.o.winwidth = 10
+    --   vim.o.winminwidth = 10
+    --   vim.o.equalalways = false
+    --   require('windows').setup()
+    -- end}
 
 
   -- Language
