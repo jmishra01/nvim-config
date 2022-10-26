df416e25a20e0bae4d69f6e108b4b945b4583926
Jitendra Mishra <jitendra29mishra@gmail.com>
2022-10-26 12:39:33 +0530

    remove comment lines

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -17,8 +17,6 @@ local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
