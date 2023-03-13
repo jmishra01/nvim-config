93a1d62815f05d8aa56d63336d75cf61a58a0037
Jitendra Mishra <jitendra29mishra@gmail.com>
2023-03-13 09:06:58 +0530

    format code

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -22,23 +22,22 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  --  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  --  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  --  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  --  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
---  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
   vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
   vim.keymap.set('n', '<space>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
@@ -89,7 +88,7 @@ require('lspconfig')['rust_analyzer'].setup {
   on_attach = on_attach,
   flags = lsp_flags,
   settings = {
-    ["rust_analyzer"] = {
+        ["rust_analyzer"] = {
       imports = { granularity = { group = "module" }, prefix = "self" },
       cargo = { buildScripts = { enable = true } },
       procMarco = { enable = true }
@@ -109,7 +108,7 @@ lspconfig.gopls.setup {
 }
 
 
-lspconfig["ccls"].setup{}
+lspconfig["ccls"].setup {}
 
 -- LUA
 local runtime_path = vim.split(package.path, ';')
@@ -137,8 +136,8 @@ lspconfig['lua_ls'].setup {
       },
       workspace = {
         library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+              [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
           vim.api.nvim_get_runtime_file("", true)
         }
       }
@@ -147,4 +146,4 @@ lspconfig['lua_ls'].setup {
 }
 
 -- Javascript
-lspconfig.tsserver.setup{}
+lspconfig.tsserver.setup {}
