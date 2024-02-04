546d36f0ab9566c33fb97c186c51955137a756a2
jitendramishra <jitendra29mishra@gmail.com>
2024-02-04 10:30:39 +0530

    add diagnostics plugin source code

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,3 +1,10 @@
 require "core.disable"
 require "core.options"
 require "core.keymaps"
+
+
+-- Disable virtual_text since it's redundant due to lsp_lines.
+vim.diagnostic.config({
+  virtual_text = false,
+})
+require("core.lsp_lines").setup()
